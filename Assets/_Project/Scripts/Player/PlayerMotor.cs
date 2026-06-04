6dafd249878a32e0aa3a95dc07178f1f2ac2907e
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-06-04 16:23:38 +0100

    Add PlayerMotor script for character movement

---
+++ Assets/_Project/Scripts/Player/PlayerMotor.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMotor : MonoBehaviour
+{
+    public float speed = 6f;
+    public float gravity = -9.81f;
+
+    CharacterController controller;
+    Vector3 velocity;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
